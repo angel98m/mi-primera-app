8a8c0a4f99b8498445ae84f0fe0bd0c53df13489
angel98m <angel98morinigo@gmail.com>
2024-06-22 05:03:16 -0400

    Mi primera App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+         <li>Learn React</li> 
+         <li>Climb Mt. Everets</li> 
+         <li>Run a marathon</li> 
+         <li>Feeds the dogs</li> 
+        </ul>
+    </div>
+  );
+}
+
+export default App;
